95768061a0b5d6f1914f1c811e5bb4b970be95ed
yocuervoc <yocuervoc@unal.edu.co>
2020-05-31 06:20:22 -0500

    links a Google Cloud

--- src/main/java/com/example/demo/EmployeeRest.java
+++ src/main/java/com/example/demo/EmployeeRest.java
@@ -9,6 +9,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.sun.el.stream.Optional;
 
 @RestController
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET, RequestMethod.POST})
 @RequestMapping("/employees")
 public class EmployeeRest {
 	
